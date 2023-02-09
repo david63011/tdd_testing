cdcb5262bdd98b8d4b1fb9e9d54cb1eb2f29da04
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2023-02-09 21:48:04 +0400

    FIZZ BUZZ TESTS

--- solver.rb
+++ solver.rb
@@ -1,14 +1,27 @@
 class Solver
-    def factorial(num)
-        if num < 0
-          return "Error"
-        elsif num == 0
-          return 1
-        else
-          return num * factorial(num - 1) 
-        end
+  def factorial(num)
+    if num < 0
+      'Error'
+    elsif num == 0
+      1
+    else
+      num * factorial(num - 1)
     end
-    def reverse (word)
-        word.is_a?(String) ? word.reverse : "Enter a string"
+  end
+
+  def reverse(word)
+    word.is_a?(String) ? word.reverse : 'Enter a string'
+  end
+
+  def fizzbuzz (num)
+    if num % 3 == 0 && num % 5 == 0
+        return "FizzBuzz"
+    elsif num % 3 == 0
+        return "Fizz"
+    elsif num % 5 == 0
+        return "Buzz"
+    else
+        return num.to_s
     end
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,55 +1,6 @@
 require_relative '../solver'
 
 describe Solver do
-<<<<<<< HEAD
-before(:each) do
-          @solver = Solver.new
-        end
-        context 'Test of the factorial method' do
-            it 'should return 1 when the given argument is 0' do
-              expect(@solver.factorial(0)).to eq 1
-            end
-        it 'should return 24 when the given argument is 4' do
-                expect(@solver.factorial(4)).to eq 24
-            end
-        it 'should return an error when the given argument is negative' do
-                expect(@solver.factorial(-1)).to eq "Error"
-                end
-          end
-          context "test the reserve method" do
-            it "should return the reversed string" do
-              expect(@solver.reverse("hello")).to eq "olleh"
-            end
-          end
-          to  Everyone
-          def fizzbuzz (num)
-                  if num % 3 == 0 && num % 5 == 0
-                      return "FizzBuzz"
-                  elsif num % 3 == 0
-                      return "Fizz"
-                  elsif num % 5 == 0
-                      return "Buzz"
-                  else
-                      return num.to_s
-                  end
-              end
-          context 'testing fizzbuzz' do
-                      it 'should return fizz when the number is divisible by 3' do
-                          expect(@solver.fizzbuzz(3)).to eq "fizz"
-                      end
-                      it 'should return buzz when the number is divisible by 5' do
-                          expect(@solver.fizzbuzz(5)).to eq "buzz"
-                      end
-                      it 'should return fizzbuzz when the number is divisible by 3 and 5' do
-                          expect(@solver.fizzbuzz(15)).to eq "fizzbuzz"
-                      end
-                      it 'should return the number when the number is not divisible by 3 or 5' do
-                          expect(@solver.fizzbuzz(2)).to eq '2'
-                      end
-                  end
-
-    end
-=======
   before(:each) do
     @solver = Solver.new
   end
@@ -61,14 +12,28 @@ before(:each) do
       expect(@solver.factorial(4)).to eq 24
     end
     it 'should return an error when the given argument is negative' do
-      expect(@solver.factorial(-1)).to eq "Error"
+      expect(@solver.factorial(-1)).to eq 'Error'
     end
   end
 
-  context "test the reserve method" do
-    it "should return the reversed string" do
-      expect(@solver.reverse("hello")).to eq "olleh"
+  context 'test the reserve method' do
+    it 'should return the reversed string' do
+      expect(@solver.reverse('hello')).to eq 'olleh'
+    end
+  end
+
+  context 'testing fizzbuzz' do
+    it 'should return fizz when the number is divisible by 3' do
+      expect(@solver.fizzbuzz(3)).to eq 'Fizz'
+    end
+    it 'should return buzz when the number is divisible by 5' do
+      expect(@solver.fizzbuzz(5)).to eq 'Buzz'
+    end
+    it 'should return fizzbuzz when the number is divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eq 'FizzBuzz'
+    end
+    it 'should return the number when the number is not divisible by 3 or 5' do
+      expect(@solver.fizzbuzz(2)).to eq '2'
     end
   end
 end
->>>>>>> 5f5c5b7ec8ac1a48cb6df3dc41f72cd1c1d6f9ea
