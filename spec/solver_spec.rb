d81cece24eeadd3848eb9718ffdb884d6f9ab90b
david63011 <david630@gmail.com>
2023-02-09 14:04:49 -0300

    add tests

---
+++ spec/solver_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../solver'
+
+describe Solver do
+    describe Solver do
+        before(:each) do
+          @solver = Solver.new
+        end
+        context 'Test of the factorial method' do
+            it 'should return 2 when the given argument is 2' do
+              expect(@solver.factorial(0)).to eq 1
+            end
+        it 'should return 24 when the given argument is 4' do
+                expect(@solver.factorial(4)).to eq 24
+            end
+        it 'should return an error when the given argument is negative' do
+                expect(@solver.factorial(-1)).to eq "Error"
+                end
+          end
+    end
